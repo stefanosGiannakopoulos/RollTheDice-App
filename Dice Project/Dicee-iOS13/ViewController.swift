e73071e4362be7b50a309fec857e3eb68df44675
Stefanos Giannakopoulos <94001087+stefanosGiannakopoulos@users.noreply.github.com>
2022-07-10 01:24:05 +0300

    Add files via upload

---
+++ Dice Project/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+   
+    @IBOutlet weak var diceImageView2: UIImageView!
+
+    var leftDiceNumber = Int.random(in: 0...5) //closed range
+    var rightDiceNumber = Int.random(in: 0...5) // closed range
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        diceImageView1.alpha = 1.0
+        diceImageView2.alpha = 1.0
+    }
+    
+   @IBAction func rollButtonPressed(_ sender: UIButton) {
+    print("Button got tapped.")
+    leftDiceNumber = Int.random(in: 0...5) //closed range
+    rightDiceNumber = Int.random(in: 0...5) //closed range
+    diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+    diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+    
+   }
+
+
+}
+
